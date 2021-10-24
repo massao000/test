f0e1f42ebe6f8b0dbd270864d5670c9c716bcbe9
massao000 <69783019+massao000@users.noreply.github.com>
2021-10-24 22:43:02 +0900

    Update web.py

--- web.py
+++ web.py
@@ -10,14 +10,14 @@ import shutil
 import streamlit as st
 
 def conversion_mp3_mp4(sound_data, file_name):
-    """
+    """ 
     sound_data : アップロードされた音声ファイル
     file_name : アップロードされた音声ファイル名
     """
     # print(os.path.splitext(file_name))
     if "mp3" in file_name:
         # print("mp3")
-        sound = AudioSegment.from_mp3(file_name)
+        sound = AudioSegment.from_file(sound_data, "mp3")
         print(sound)
         return sound, io.BufferedRandom(sound.export(format="wav"))
     elif "mp4" in file_name:
@@ -40,23 +40,22 @@ if file:
     start_one = st.button("①開始")
     if start_one == True:
         conversion = conversion_mp3_mp4(file, file.name)
-        print(conversion[0])
-        print(conversion[1])
+        # print(conversion[0])
+        # print(conversion[1])
         chunks = split_on_silence(conversion[0], min_silence_len=2000, silence_thresh=-40, keep_silence=1000)
-        print(chunks)
+        # print(chunks)
         z = [ io.BufferedRandom(chunk.export(format="wav")) for i, chunk in enumerate(chunks)]
-        print(z)
+        # print(z)
         r = sr.Recognizer()
         texts = []
         for i in z:
-            print(i)
             with sr.AudioFile(i) as source:
 
                 audio = r.record(source)
 
             text = r.recognize_google(audio, language='ja-JP', show_all=False)
             texts.append(text)
-        text = "、".join(texts)
+        text = "\n".join(texts)
         st.write(text)
         
         contents_one = f"ファイルから文字起こしファイルから文字起こしファイルから文字起こし"
