b4b34de7d8a43793b5f03a83e8d1bdd628539fd4
massao000 <69783019+massao000@users.noreply.github.com>
2022-01-31 01:45:36 +0900

    Update web.py

--- web.py
+++ web.py
@@ -13,6 +13,12 @@ import numpy
 import os
 import glob
 
+
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
+from streamlit_bokeh_events import streamlit_bokeh_events
+
+
 # 自然数ソート
 from natsort import natsorted
 
@@ -291,3 +297,35 @@ if start_two:
                     # placeholder.info(contents_two) # ボックス追加
                     contents_two = f"{now} {contents_two}"
                     texts.append(contents_two)
+                 
+                
+
+
+stt_button = Button(label="Speak", width=100)
+
+stt_button.js_on_event("button_click", CustomJS(code="""
+    var recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
+ 
+    recognition.onresult = function (e) {
+        var value = "";
+        for (var i = e.resultIndex; i < e.results.length; ++i) {
+            if (e.results[i].isFinal) {
+                value += e.results[i][0].transcript;
+            }
+        }
+        if ( value != "") {
+            document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
+        }
+    }
+    recognition.start();
+    """))
+
+result = streamlit_bokeh_events(
+    stt_button,
+    events="GET_TEXT",
+    key="listen",
+    refresh_on_update=False,
+    override_height=75,
+    debounce_time=0)
